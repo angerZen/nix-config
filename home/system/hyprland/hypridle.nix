26055f8eb88a0c2c4dbd2ce5feea938479802d1c
angerZen <67027795+angerZen@users.noreply.github.com>
2024-09-21 08:36:45 -0500

    wallpaper updates

--- home/system/hyprland/hypridle.nix
+++ home/system/hyprland/hypridle.nix
@@ -1,18 +1,30 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  suspendScript = pkgs.writeShellScript "suspend-script" ''
+    # check if any player has status "Playing"
+    ${lib.getExe pkgs.playerctl} -a status | ${lib.getExe pkgs.ripgrep} Playing -q
+    # only suspend if nothing is playing
+    if [ $? == 1 ]; then
+      ${pkgs.systemd}/bin/systemctl suspend
+    fi
+  '';
+in {
   services.hypridle = {
     enable = true;
     settings = {
       general = {ignore_dbus_inhibit = false;};
       listener = [
         {
-          timeout = 600;
+          timeout = 300;
           on-timeout = pkgs.hyprlock + "/bin/hyprlock";
         }
 
         {
-          timeout = 605;
-          on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
-          on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+          timeout = 600;
+          on-timeout = suspendScript.outPath;
         }
       ];
     };
