d11b166dcecd95b735cf29c87dcc2116a6e333c6
angerZen <67027795+angerZen@users.noreply.github.com>
2025-03-29 20:10:31 -0500

    syntax

--- hosts/modules/home-manager.nix
+++ hosts/modules/home-manager.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   home-manager = {
-    useGlobalPackages = true;
+    useGlobalPkgs = true;
     useUserPackages = true;
     extraSpecialArgs = {inherit inputs;};
     backupFileExtension = "backup-" + pkgs.lib.readFile "${pkgs.runCommand "timestamp" {} "echo -n `date '+%Y%m%d%H%M%S'` > $out"}";
