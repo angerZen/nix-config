4cc509335abfe0d08b4160d5981c5d1270193cf5
angerZen <67027795+angerZen@users.noreply.github.com>
2025-03-29 20:01:43 -0500

    more syntax

--- hosts/modules/nix.nix
+++ hosts/modules/nix.nix
@@ -2,7 +2,7 @@
   config,
   ...
 }: {
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreePredicate = _: true;
   nixpkgs.config.permittedInsecurePackages = [
      "openssl-1.1.1w"
    ];
