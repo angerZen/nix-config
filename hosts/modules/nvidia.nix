9d19c4be3be816f902903479f3ff458dd6ead33e
angerZen <67027795+angerZen@users.noreply.github.com>
2025-04-27 11:06:56 -0500

    updates

--- hosts/modules/nvidia.nix
+++ hosts/modules/nvidia.nix
@@ -22,7 +22,7 @@
       powerManagement.enable = true; # This can cause sleep/suspend to fail and saves entire VRAM to /tmp/
       forceFullCompositionPipeline = true;
       modesetting.enable = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
     };
   };
 }
