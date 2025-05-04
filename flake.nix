0af86711ae3fbd5e80d163f87e747583dbb97afd
angerZen <67027795+angerZen@users.noreply.github.com>
2025-05-03 20:33:47 -0500

    removed cosmic

--- flake.nix
+++ flake.nix
@@ -20,11 +20,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     nix-citizen.url = "github:LovingMelody/nix-citizen";
 
     # Optional - updates underlying without waiting for nix-citizen to update
